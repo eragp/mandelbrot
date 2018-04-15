292f0eb9a63b616ab80e863915a4cc65ad6c35ec
Lercher <lercher@singhammer.de>
2018-04-15 22:14:47 +0200

    Add rank

--- MPIHelloWorld/main.cpp
+++ MPIHelloWorld/main.cpp
@@ -12,13 +12,16 @@ int main(int argc, char** argv) {
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
     char processorname[MPI_MAX_PROCESSOR_NAME];
     int name_len;
     MPI_Get_processor_name(processorname, &name_len);
 
     cout << "Hello!" << endl;
     cout << processorname << endl;
-    cout << "Size: " << world_size << endl;
+    cout << "Size: " << world_size << endl << "Rank: " << rank <<endl;
 
     MPI_Finalize();
     return 0;
