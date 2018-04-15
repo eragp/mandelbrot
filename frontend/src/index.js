7951f89ccf78be6f15261ea662588d5bc0a96736
Max Fr <fruehaufmaximilian@gmail.com>
2018-04-15 20:31:47 +0200

    add eslint support

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
-import "leaflet/dist/leaflet.css";
-import "./index.css";
-import L from "leaflet";
+import 'leaflet/dist/leaflet.css';
+import './index.css';
+import L from 'leaflet';
 
 // demo loading the london city map
-let londonMap = L.map("map01").setView([51.505, -0.09], 13);
-L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "OpenStreetMaps",
+let londonMap = L.map('map01').setView([51.505, -0.09], 13);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'OpenStreetMaps',
 }).addTo(londonMap);
+
+console.log('This is a test');
