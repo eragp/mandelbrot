5eba0f5e48ca544caf6f007cc8ce913d95aafdcd
Max Fruehauf <max.fruehauf@tum.de>
2018-04-23 00:57:39 +0200

    add mpich to mandelbrot-base layer

--- backend/dockerLibBaseImage/build.sh
+++ backend/dockerLibBaseImage/build.sh
@@ -2,7 +2,7 @@
 
 # this file builds the libDockerfile and publishes it to dockerHub
 ## the ENV var DOCKER_ID_USER has to be set ##
-version="0.2"
+version="0.3"
 
 docker login
 docker build --tag mandelbrot:$version .
