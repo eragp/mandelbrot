cf846e73654874f9b88acb9678998442214b7116
Max Fruehauf <max.fruehauf@tum.de>
2018-04-22 13:19:14 +0200

    Cache libs image layer on DockerHub

---
+++ backend/dockerLibBaseImage/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# this file builds the libDockerfile and publishes it to dockerHub
+## the ENV var DOCKER_ID_USER has to be set ##
+version="0.2"
+
+docker login
+docker build --tag mandelbrot:$version .
+docker tag mandelbrot:$version $DOCKER_ID_USER/mandelbrot
+docker push $DOCKER_ID_USER/mandelbrot
