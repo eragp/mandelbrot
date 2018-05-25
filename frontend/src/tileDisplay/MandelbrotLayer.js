1a04987b8303162f3f3612c2612f689a3f26e205
Max Fruehauf <max.fruehauf@tum.de>
2018-05-25 16:26:51 +0200

    request tiles directly with Imaginary Coordinates

--- frontend/src/tileDisplay/MandelbrotLayer.js
+++ frontend/src/tileDisplay/MandelbrotLayer.js
@@ -1,39 +1,6 @@
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
-L.GridLayer.MandelbrotLayer = L.GridLayer.extend({
-  createTile: function(coords) {
-    // var error;
-    var tile = L.DomUtil.create('canvas', 'leaflet-tile');
-    var size = this.getTileSize();
-    tile.width = size.x;
-    tile.height = size.y;
-    var url =
-      'http://localhost:8080/mandelbrot?' +
-      'x=' +
-      coords.x +
-      '&y=' +
-      coords.y +
-      '&z=' +
-      coords.z +
-      '&size=' +
-      size.x;
-    console.log(coords);
-    // draw a moire pattern to the tile
-    let ctx = tile.getContext('2d');
-    ctx.clearRect(0, 0, size.x, size.y);
-    let px = ctx.createImageData(1, 1);
-    for (let x = 0; x < size.x; x++) {
-      for (let y = 0; y < size.y; y++) {
-        let value = (x * y) & 0xff;
-        ctx.fillStyle =
-          'rgba(' + value + ',' + value + ',' + value + ',' + 255 + ')';
-        ctx.fillRect(x, y, 1, 1);
-      }
-    }
-    return tile;
-  }
-});
 
 // const renderTile = async () => {
 //   console.log('Requesting: ' + url);
--- frontend/src/tileDisplay/TileDisplay.js
+++ frontend/src/tileDisplay/TileDisplay.js
@@ -17,22 +17,64 @@ class TileDisplay extends Component {
 }
 
 const renderLeaflet = () => {
+  L.GridLayer.MandelbrotLayer = L.GridLayer.extend({
+    createTile: function(coords) {
+      // var error;
+      // var tile = L.DomUtil.create('canvas', 'leaflet-tile');
+      var tile = document.createElement('div');
+      const fromLatLng = map.unproject(coords, coords.z);
+      const toLatLng = map.unproject(
+        L.point(coords.x + 1, coords.y + 1),
+        coords.z
+      );
+      var size = this.getTileSize();
+      tile.width = size.x;
+      tile.height = size.y;
+      var url =
+        'http://localhost:8080/mandelbrot?' +
+        'fromReal=' +
+        fromLatLng.lng.toString() +
+        '&fromImag=' +
+        fromLatLng.lat.toString() +
+        '&toReal=' +
+        toLatLng.lng.toString() +
+        '&toImag=' +
+        toLatLng.lat.toString();
+      console.log(url);
+      tile.innerHTML =
+        'from: ' + fromLatLng + '<br> to: ' + toLatLng + '<br>' + url;
+      tile.style.outline = '1px solid red';
+      // console.log(map.layerPointToLatLng(coords));
+      // draw a moire pattern to the tile
+      // let ctx = tile.getContext('2d');
+      // ctx.clearRect(0, 0, size.x, size.y);
+      // // let px = ctx.createImageData(1, 1);
+      // for (let x = 0; x < size.x; x += 4) {
+      //   for (let y = 0; y < size.y; y += 4) {
+      //     let value = x | y % 256;
+      //     ctx.fillStyle =
+      //       'rgba(' + value + ',' + value + ',' + value + ',' + 255 + ')';
+      //     ctx.fillRect(x, y, 4, 4);
+      //   }
+      // }
+      return tile;
+    }
+  });
+  const scale = 100;
   L.gridLayer.mandelBrotLayer = () =>
     new L.GridLayer.MandelbrotLayer({
       tileSize: 256, // in px
-      bounds: [[0, 0], [1000, 1000]]
+      bounds: [[-2 * scale, -2 * scale], [2 * scale, 2 * scale]]
     });
-
-  let map = L.map('viewer', {
+  var map = L.map('viewer', {
     crs: L.CRS.Simple,
-    // minZoom: 2,
-    center: [500, 500],
+    minZoom: 1,
+    center: [0, 0],
     zoom: 1
   });
   map.addLayer(L.gridLayer.mandelBrotLayer());
   // L.geoJSON(generate(), { style: styleUsageMap }).addTo(map);
 };
-
 
 // const styleUsageMap = feature => {
 //   return {
