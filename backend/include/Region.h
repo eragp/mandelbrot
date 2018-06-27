7b0a510ee74941376100b1427b614862854b97a7
Florian Lercher <ga27col@mytum.de>
2018-06-27 18:39:42 +0200

    Fixed same typos in Region.h

--- backend/include/Region.h
+++ backend/include/Region.h
@@ -1,50 +1,51 @@
 /**
- * The new and up-to-date Region struct for view and subdivided regions
- */
+* The new and up-to-date Region struct for view and subdivided regions
+*/
 struct Region {
 
-    /**
-     * The coordinates for the top left corner of the region
-     * in real values. 
-     * Included in the region.
-     */
-    long double minReal, maxImaginary;
+	/**
+	* The coordinates for the top left corner of the region
+	* in the complex plane.
+	* Included in the region.
+	*/
+	long double minReal, maxImaginary;
 
-    /**
-     * The coordinates for the bottom right corner of the region
-     * in real values.
-     * Excluded from the region.
-     */
-    long double maxReal, minImaginary;
+	/**
+	* The coordinates for the bottom right corner of the region
+	* in the complex plane.
+	* Excluded from the region.
+	*/
+	long double maxReal, minImaginary;
 
-    /**
-     * Pixel amount in respective dimension of this region.
-     * Equivalent to resolution in x- and y- dimension.
-     */
-    unsigned int width, height;
+	/**
+	* Pixel amount in respective dimension of this region.
+	* Equivalent to resolution in x- and y- dimension.
+	*/
+	unsigned int width, height;
 
-    /**
-     * Maximum n value for iuteration in this region.
-     */
-    unsigned int maxIteration;
+	/**
+	* Maximum n value for iteration in this region.
+	*/
+	unsigned int maxIteration;
 
-    /**
-     * Forntend specific information, identification/ validation value value, do not touch
-     * With leaflet frontend equivalent to zoomfactor.
-     */
-    int validation;
+	/**
+	* Frontend specific information, identification/validation value, do not touch
+	* With leaflet frontend equivalent to zoomfactor.
+	*/
+	int validation;
 
-    /**
-     * Value for which it is guaranteed that widht and height are divisible by.
-     * Recursivley applies to all subregions
-     */
-    unsigned int guaranteedDivisor; 
+	/**
+	* Value for which it is guaranteed that width and height are divisible by.
+	* Same goes for hOffset and vOffset
+	* Recursivley applies to all subregions
+	*/
+	unsigned int guaranteedDivisor;
 
-    /**
-     * Horizonatl and vertical offset respective to superregion.
-     * Equivalent to xOffset/yOffset.
-     * Should not become negative usually.
-     */
-    int hOffset, vOffset;
+	/**
+	* Horizonatl and vertical offset respective to superregion.
+	* Equivalent to xOffset/yOffset.
+	* Should not become negative usually.
+	*/
+	int hOffset, vOffset;
 
 };
