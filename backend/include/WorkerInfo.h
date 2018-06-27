08b42270924812b49f08ccb1f99522b93727b162
Niels Mündler <n.muendler@tum.de>
2018-06-27 17:37:33 +0200

    Add workerinfo struct
    Added region information so that the host is not obliged to store a worker->region mapping anymore or could validate this if necessary

---
+++ backend/include/WorkerInfo.h
@@ -0,0 +1,23 @@
+#include "Region.h"
+
+/**
+ * Information sent by Worker when done with computing data.
+ * Contains constant amount of metadata.
+ */
+struct WorkerInfo {
+
+    /**
+     * The rank of the worker, that has computed the following data
+     */
+    int rank;
+
+    /**
+     * The time that computation has taken in nanoseconds
+     */
+    long long computationTime;
+
+    /**
+     * The region that was computed.
+     */
+    Region region;
+};
