538678d23db3715e34c34d751fb60888ce39974a
Niels Mündler <n.muendler@tum.de>
2018-06-27 21:44:18 +0200

    Removed http/ Rest artifacts

--- backend/include/Host.h
+++ backend/include/Host.h
@@ -19,37 +19,15 @@ public:
     static void init(int worldRank, int worldSize);
 
 private:
-    // Static da als handle übergeben werden muss
-    // => ganzer Rest auch static, lediglich Informationskapselung
-	
-	static void handle_get_tile(web::http::http_request request);
-
-    static void handle_get_region(web::http::http_request request);
-
-    static void answer_requests(RegionOld requested_tile);
+    // Static, as currently method handles are passed without bind
 
     static int maxIteration;
     static int world_size;
-    // Dictionary for storing requests for a given tile
-    static std::map<Tile, std::vector<web::http::http_request>> request_dictionary;
-    static std::mutex request_dictionary_lock;
 
     // Store for the current big tile
     static RegionOld current_big_tile;
     static std::mutex current_big_tile_lock;
     // And for the split up regions
-
-    // Keeps track of all available cores
-    static std::queue<int> avail_cores;
-    static std::mutex avail_cores_lock;
-
-    // Store requests
-    static std::queue<RegionOld> requested_regions;
-    static std::mutex requested_regions_lock;
-
-    // And answers
-    static std::map<Tile, TileData> available_tiles;
-    static std::mutex available_tiles_lock;
 
     //Store send MPI Requests
     static std::map<int, RegionOld> transmitted_regions;
