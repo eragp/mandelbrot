2664dbcfed4a30301cd3283136ee8ee7f9e1af1a
Florian Lercher <ga27col@mytum.de>
2018-06-29 23:35:01 +0200

    ColumnBalancer now using new Region struct

--- backend/src/balancer/ColumnBalancer.cpp
+++ backend/src/balancer/ColumnBalancer.cpp
@@ -1,29 +1,48 @@
 #include "ColumnBalancer.h"
 #include <cmath>
 #include "Region.h"
+#include "Fractal.h"
 
 /**
  * IntergerBalancer takes a region and separates it into #nodeCount
  * columns tile sets. (eg. region is split into #nodeCount columns)
  */
 Region* ColumnBalancer::balanceLoad(Region region, const int nodeCount) {
-    auto regions = new Region[nodeCount];
-    //int regionWidth = region.getWidth();
-    //int columnWidth = regionWidth / nodeCount;
-    //int x = region.tlX;
-    //for (int i = 0; i < nodeCount; i++) {
-    //    RegionOld r{};
-    //    regions[i] = r;
-    //    regions[i].tlX = x;
-    //    regions[i].brX = (i == nodeCount - 1) ? region.brX : (x + columnWidth);
-    //    // just copying the unchanged values for each RegionOld struct
-    //    regions[i].tlY = region.tlY;
-    //    regions[i].brY = region.brY;
-    //    regions[i].maxIteration = region.maxIteration;
-    //    regions[i].resX = region.resX;
-    //    regions[i].resY = region.resY;
-    //    regions[i].zoom = region.zoom;
-    //    x += columnWidth + 1;
-    //}
+    Region* regions = new Region[nodeCount];
+
+    int columnWidth = (region.width / (region.guaranteedDivisor * nodeCount)) * region.guaranteedDivisor;
+    int xDelta = Fractal::deltaReal(region.maxReal, region.minReal, region.width) * columnWidth;
+
+    Region tmp;
+	// These stay the same over all iterations
+	tmp.minImaginary = region.minImaginary;
+	tmp.maxImaginary = region.maxImaginary;
+	tmp.height = region.height;
+	tmp.maxIteration = region.maxIteration;
+	tmp.vOffset = region.vOffset;
+	tmp.validation = region.validation;
+	tmp.guaranteedDivisor = region.guaranteedDivisor;
+
+	// This will change, so will width and maxReal
+	tmp.minReal = region.minReal;
+	tmp.hOffset = region.hOffset;
+
+    for (int i = 0; i < nodeCount; i++) {
+       if (i == nodeCount - 1) {
+           // Take the rest
+           tmp.maxReal = region.maxReal;
+		   tmp.width = region.width - columnWidth * i;
+           regions[i] = tmp;
+       } else {
+           tmp.maxReal = tmp.minReal + xDelta;
+           tmp.width = columnWidth;
+
+           regions[i] = tmp;
+           // Update minReal and hOffset
+           tmp.minReal = tmp.maxReal;
+           tmp.hOffset += tmp.width;
+       }
+    }
+    
     return regions;
 }
