ce7cbfce141bc6239073e550b23183ab5e63dd18
Niels Mündler <n.muendler@tum.de>
2018-07-02 02:07:09 +0200

    Works, needs heavy refactoring and region->tile mapping not yet complete

--- frontend/src/connection/WSClient.js
+++ frontend/src/connection/WSClient.js
@@ -1,82 +1,90 @@
-/**
- * Callbacks for any methods interested in new region subdivisions or regionData (=result of one worker)
- */
-const regionCallback = [];
-const workerCallback = [];
+export default class {
+  constructor(){
+    /**
+     * Callbacks for any methods interested in new region subdivisions or regionData (=result of one worker)
+     */
+    this.regionCallback = [];
+    let regionCallback = this.regionCallback;
+    this.workerCallback = [];
+    let workerCallback = this.workerCallback;
 
-// Web Socket setup
-const url = 'ws://localhost:9002';
-const socket = new WebSocket(url); //, 'mandelbrot');
-// Buffer of requests to be sent when the socket connects
-const regionRequests = [];
-socket.onopen = () => {
-  regionRequests.forEach(m => socket.send(m));
-};
+    // Web Socket setup
+    let url = 'ws://localhost:9002';
+    let socket = new WebSocket(url); //, 'mandelbrot');
+    // Buffer of requests to be sent when the socket connects
+    this.regionRequests = [];
+    let regionRequests = this.regionRequests;
+    socket.onopen = () => {
+      regionRequests.forEach(m => socket.send(m));
+    };
 
-socket.onmessage = function(event) {
-  let msg = JSON.parse(event.data);
-  console.log(msg);
-  switch (msg.type) {
-    case 'regionData':
-      // Notify regionData/worker observers
-      workerCallback.forEach(callback => {
-        callback(msg);
-      });
-      break;
-    case 'regions':
-      // Notify region subdivision listeners
-      regionCallback.forEach(callback => {
-        callback(msg);
-      });
-      break;
-    default:
-  }
-};
-
-/**
- * Registers a callback to call when the region subdivision is returned
- */
-export const registerRegion = fun => {
-  registerCallback(regionCallback, fun);
-};
-
-/**
- * Registers a callback to call when the region data is returned
- */
-export const registerWorker = fun => {
-  registerCallback(workerCallback, fun);
-};
-
-/**
- * Registers an observer to a list
- */
-const registerCallback = (list, fun) => {
-  let promise;
-  const render = data => {
-    promise = new Promise((resolve, error) => {
-      try {
-        resolve(fun(data));
-      } catch (err) {
-        error(err);
+    socket.onmessage = function(event) {
+      let msg = JSON.parse(event.data);
+      console.log(msg);
+      switch (msg.type) {
+        case 'regionData':
+          // Notify regionData/worker observers
+          workerCallback.forEach(callback => {
+            callback(msg);
+          });
+          break;
+        case 'regions':
+          // Notify region subdivision listeners
+          regionCallback.forEach(callback => {
+            callback(msg);
+          });
+          break;
+        default:
       }
-    });
-  };
-  list.push(render);
-  return promise;
-};
+    };
 
-export const close = () => {
-  console.log('closing the WS connection');
-  socket.close();
-};
-
-export const sendRequest = request => {
-  let message = JSON.stringify(request);
-  if (socket.readyState === socket.OPEN) {
-    socket.send(message);
-  } else {
-    regionRequests.push(message);
+    this.socket = socket;
   }
-};
 
-export default socket;
+  /**
+   * Registers a callback to call when the region subdivision is returned
+   */
+  registerRegion(fun){
+    this.registerCallback(this.regionCallback, fun);
+  }
+
+  /**
+   * Registers a callback to call when the region data is returned
+   */
+  registerWorker(fun){
+    this.registerCallback(this.workerCallback, fun);
+  }
+
+  /**
+   * Registers an observer to a list
+   */
+  registerCallback(list, fun){
+    let promise;
+    const render = data => {
+      promise = new Promise((resolve, error) => {
+        try {
+          resolve(fun(data));
+        } catch (err) {
+          error(err);
+        }
+      });
+    };
+    list.push(render);
+    return promise;
+  }
+
+  close(){
+    console.log('closing the WS connection');
+    this.socket.close();
+  }
+
+  sendRequest(request){
+    let message = JSON.stringify(request);
+    if (this.socket.readyState === this.socket.OPEN) {
+      this.socket.send(message);
+    } else {
+      this.regionRequests.push(message);
+    }
+  }
+
+}
--- frontend/src/tileDisplay/RegionDrawer.js
+++ frontend/src/tileDisplay/RegionDrawer.js
@@ -1,105 +1,110 @@
-import { registerWorker, registerRegion } from '../connection/WSClient';
-import { registerNewRegion } from './TileDisplay';
-// import { currentTopLeft, currentBottomRight } from './RegionRequest';
 import Point from '../misc/Point';
+import { tileSize } from './Constants';
+import { unproject } from './Project';
 
-/*
- * this map stores callbacks to render all the tiles requested for leaflet.
- */
-const callbacks = new Map();
-/**
- * These variables handle the current visible region bounds as points
- */
-var topLeft;
-var bottomRight;
+export default class {
 
-/**
- * Registers the tile at coords to be drawn as soon as data is available.
- * @param {*} point coordinates on the tile to be registerd
- * @param {*} draw function expecting data that draws the tile @coords
- */
-export const register = (point, draw) => {
-  let promise;
-  const render = data => {
-    promise = new Promise((resolve, error) => {
-      try {
-        draw(data);
-        resolve();
-      } catch (err) {
-        error(err);
+  constructor(tileDisplay, webSocketClient){
+    /*
+    * this map stores callbacks to render all the tiles requested for leaflet.
+    */
+    this.callbacks = new Map();
+    /**
+      * These variables handle the current visible region bounds as points
+      */
+    this.topLeft;
+    this.bottomRight;
+
+    let handleRegionData = msg => {
+      let region = msg.workerInfo.region;
+      let zoom = region.validation;
+      // compute x/y coordinates based on region
+      let tileSize = region.guaranteedDivisor;
+      let xEnd = region.width / tileSize;
+      let yEnd = region.height / tileSize;
+
+      let topLeft = unproject(region.minReal, region.maxImag, zoom);
+  
+      // and invoke tile draw methods
+      for (let y = 0; y < yEnd; y++) {
+        for (let x = 0; x < xEnd; x++) {
+          let tileX = topLeft.x + x;
+          let tileY = topLeft.y + y;
+          let cb = this.callbacks.get(coordsToString(tileX, tileY, zoom));
+          if (cb === undefined || cb === null) {
+            console.log('Region not found for ' + new Point(tileX, tileY, zoom));
+            continue;
+          }
+          // only pass data of this region
+          // TODO test
+          let realX = x * tileSize;
+          let realY = y * tileSize;
+          let tl = new Point(realX, realY);
+          let br = new Point(realX + tileSize, realY + tileSize);
+          let roi = new RegionOfInterest(
+            tl,
+            br,
+            msg.data,
+            region.width,
+            region.height
+          );
+          cb(roi);
+        }
       }
-    });
-  };
-  let coords = coordsToString(point.x, point.y, point.z);
-  callbacks.set(coords, render);
-  return promise;
-};
-
-const handleRegionData = msg => {
-  let region = msg.workerInfo.region;
-  let zoom = msg.validation;
-  // compute x/y coordinates based on region
-  let tileSize = region.guaranteedDivisor;
-  let xStart = region.hOffset / tileSize;
-  let xEnd = xStart + region.width / tileSize;
-  let yStart = region.vOffset / tileSize;
-  let yEnd = yStart + region.height / tileSize;
-
-  // and invoke tile draw methods
-  for (let y = yStart; y < yEnd; y++) {
-    for (let x = xStart; x < xEnd; x++) {
-      let tileX = topLeft.x + x;
-      let tileY = topLeft.y + y;
-      let cb = callbacks.get(coordsToString(tileX, tileY, zoom));
-      if (cb === null) {
-        console.log('Region not found for ' + new Point(x, y, zoom));
-        continue;
-      }
-      // only pass data of this region
-      // TODO test
-      let realX = x * tileSize;
-      let realY = y * tileSize;
-      let tl = new Point(realX, realY);
-      let br = new Point(realX + tileSize, realY + tileSize);
-      let roi = new RegionOfInterest(
-        tl,
-        br,
-        msg.data,
-        region.width,
-        region.height
-      );
-      cb(roi);
     }
+  
+    let handleNewRegion = map => {
+      let bounds = map.getPixelBounds();
+      let zoom = map.getZoom();
+      let tileSize = tileSize;
+      // aka top left
+      this.topLeft = new Point(
+        Math.floor(bounds.min.x / tileSize),
+        -Math.floor(bounds.min.y / tileSize),
+        zoom
+      );
+      // aka bottom right
+      this.bottomRight = new Point(
+        Math.floor(bounds.max.x / tileSize),
+        -Math.floor(bounds.max.y / tileSize),
+        zoom
+      );
+    };
+    /**
+     * Register yourself at WSClient as regionData observer
+     */
+    webSocketClient.registerWorker(handleRegionData);
+
+    /**
+     * Also observe the current region
+     */
+    tileDisplay.registerNewView(handleNewRegion);
   }
-};
+  
 
-const handleNewRegion = map => {
-  let bounds = map.getPixelBounds();
-  let zoom = map.getZoom();
-  let tileSize = map.getTileSize();
-  // aka top left
-  topLeft = new Point(
-    Math.floor(bounds.min.x / tileSize),
-    -Math.floor(bounds.min.y / tileSize),
-    zoom
-  );
-  // aka bottom right
-  bottomRight = new Point(
-    Math.floor(bounds.max.x / tileSize),
-    -Math.floor(bounds.max.y / tileSize),
-    zoom
-  );
-};
+  /**
+   * Registers the tile at coords to be drawn as soon as data is available.
+   * @param {*} point coordinates on the tile to be registerd
+   * @param {*} draw function expecting data that draws the tile @coords
+   */
+  register(point, draw){
+    let promise;
+    const render = data => {
+      promise = new Promise((resolve, error) => {
+        try {
+          draw(data);
+          resolve();
+        } catch (err) {
+          error(err);
+        }
+      });
+    };
+    let coords = coordsToString(point.x, point.y, point.z);
+    this.callbacks.set(coords, render);
+    return promise;
+  };
 
-/**
- * Register yourself at WSClient as regionData observer
- */
-registerWorker(handleRegionData);
-
-/**
- * Also observe the current region
- */
-registerNewRegion(handleNewRegion);
+}
 
 /**
  * Represents a view on the data of the rectangle defined by tl, br.
@@ -119,8 +124,8 @@ class RegionOfInterest {
     this.width = width;
     this.height = height;
 
-    this.ROIWidth = topLeft.x - bottomRight.x;
-    this.ROIHeight = topLeft.y - bottomRight.y;
+    this.ROIWidth = tl.x - br.x;
+    this.ROIHeight = tl.y - br.y;
   }
 
   /**
@@ -128,10 +133,10 @@ class RegionOfInterest {
    * by wrapping x and y in the rectangle of topLeft, bottomRight.
    */
   get(x, y) {
-    if (x > this.ROIWidth || y > this.ROIHeight) {
+    /*if (x > this.ROIWidth || y > this.ROIHeight) {
       console.log('Illegal access');
       return -1;
-    }
+    }*/
     let realX = this.topLeft.x + x;
     let realY = this.topLeft.y + y;
     return this.data[realY * this.width + realX];
