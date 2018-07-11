58a89fb5d7d2a1dd84896e7bf08267660192a94f
Niels Mündler <n.muendler@tum.de>
2018-07-11 22:46:57 +0200

    Include dynamic progress, still errors when drawing (does not appear after new dataset

--- frontend/src/visualization/NodeProgress.js
+++ frontend/src/visualization/NodeProgress.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
+import { Doughnut } from 'react-chartjs-2';
 
 export default class extends Component {
 
-    componentDidMount() {
+    constructor(props){
+        super(props);
+        this.websocketClient = props.wsclient;
+    }
 
-        let websocketClient = this.props.wsclient;
-        // TODO move outside => into NodeList?
+    componentWillMount() {
+        this.setState({
+            numWorkers: 0,
+            active: [false],
+            // The computation time in microseconds
+            progress: [1]
+          });
+    }
+
+    componentDidMount() {
+        console.log(this.refs.chart.chart_instance);
+        // Interval in milliseconds
+        let interval = 500;
+        let _this = this;
+        this.interval = setInterval(
+            () => {      
+                _this.setState((oldState) => {
+                    for(let i = 0; i < oldState.progress.length; i++){
+                        if(oldState.active[i]){
+                            oldState.progress[i] += interval * 1000;
+                        }
+                    }
+                    return oldState;
+                });
+            },
+            interval
+        );
 
         // register workers at websocket client
         // so that they are set inactive when the first tile/region
         // by them comes in
-        websocketClient.registerWorker((data) => {
+        this.websocketClient.registerWorker((data) => {
             // Stop corresponding worker progress bar
             // assume that regionData is passed here
             let workerID = data.workerInfo.rank;
@@ -22,7 +51,7 @@ export default class extends Component {
             });
         });
         
-        websocketClient.registerRegion((data) => {
+        this.websocketClient.registerRegion((data) => {
             // Draw node progress
             let nworkers = data.regionCount;
             let active = new Array(nworkers);
@@ -32,16 +61,31 @@ export default class extends Component {
                 progress[i] = 0;
             }
             this.setState({
-            numWorkers: nworkers,
-            active: active,
-            progress: progress
+                numWorkers: nworkers,
+                active: active,
+                progress: progress
             });
-            // TODO draw regions
         });
     }
 
     render(){
-        return <div>hello!</div>;
+        let progress = this.state.progress;
+        let labels = [];
+        for(let i = 0; i < progress.length; i++){
+            labels.push("Worker " + i);
+        }
+        let data = {
+            labels: labels,
+            datasets: [{
+                data: progress
+            }]
+        };
+        let options = {
+            responsive: false
+        };
+        console.log(data);
+        this.reference = null;
+        return (<div><Doughnut ref='chart' data={data} options={options}/></div>);
     }
     
 }
