bab0af856da1e4853bc2571806376acee88fc160
Florian Lercher <ga27col@mytum.de>
2018-08-06 15:07:58 +0200

    Increase MPI thread safety level

--- backend/src/init.cpp
+++ backend/src/init.cpp
@@ -17,7 +17,15 @@ const int default_res = 256;
 int init(int argc, char **argv, bool host) {
     const char* type = host ? "Host" : "Worker";
 
-    MPI_Init(&argc, &argv);
+    int providedMPIThreadLevel;
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &providedMPIThreadLevel);
+
+    if (providedMPIThreadLevel < MPI_THREAD_MULTIPLE) {
+        std::cerr << "MPI thread level support is insufficient." << std::endl;
+        MPI_Abort(MPI_COMM_WORLD, 1);
+        return -1;  // Return with error
+    }
+
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     int world_size;
@@ -31,7 +39,7 @@ int init(int argc, char **argv, bool host) {
         " on node " << proc_name << std::endl;
 
     if (world_size < 2) {
-       std:: cerr << "Need at least 2 processes to run. Currently have " << world_size << std::endl;
+       std::cerr << "Need at least 2 processes to run. Currently have " << world_size << std::endl;
         MPI_Finalize();
         return -1;  // return with error
     }
