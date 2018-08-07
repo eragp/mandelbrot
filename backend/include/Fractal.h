1a31169b8678474691e1fa63aae50d0bafb892e3
Florian Lercher <ga27col@mytum.de>
2018-08-07 13:10:26 +0200

    Clean up Fractal class

--- backend/include/Fractal.h
+++ backend/include/Fractal.h
@@ -4,11 +4,6 @@ class Fractal {
     virtual int calculateFractal(long double cReal, long double cImaginary, int maxIteration) = 0;
     virtual ~Fractal();
 
-    //TODO is this still necessary? -> Yes, except xToReal und yToReal in the future. (@Florian uses them in a Balancer; He will change that)
     static double deltaReal(double maxReal, double minReal, int xRes);
     static double deltaImaginary(double maxImaginary, double minImaginary, int yRes);
-    static long double xToReal(long x, long zoom, long localX, long size);
-    static long double yToImaginary(long y, long zoom, long localY, long size);
-    // static int realToX(double real, double maxReal, double minReal, int width);
-    // static int imaginaryToY(double imaginary, double maxImaginary, double minImaginary, int height);
 };
--- backend/src/mandelbrot/Fractal.cpp
+++ backend/src/mandelbrot/Fractal.cpp
@@ -1,8 +1,5 @@
 #include "Fractal.h"
 
-const long double boundX = 4;
-const long double boundY = 4;
-
 Fractal::~Fractal() {}
 
 double Fractal::deltaReal(double maxReal, double minReal, int xRes) {
@@ -11,28 +8,5 @@ double Fractal::deltaReal(double maxReal, double minReal, int xRes) {
 
 double Fractal::deltaImaginary(double maxImaginary, double minImaginary, int yRes) {
     return (maxImaginary - minImaginary) / yRes;
-}
-
-
-/**
- * unprojects from Tile coordinate space to the complex plane.
- *
- */
-long double unproject(long x, long zoom, double bound, long local, long size) {
-    if (size == 0) {
-        size = 1;
-    }
-    long long tile_count = static_cast<unsigned long long>(1) << static_cast<unsigned long long>(zoom);
-    return (long double) (x * bound * size + local * bound) /
-           (long double) (tile_count * size);
-}
-
-
-long double Fractal::xToReal(long x, long zoom, long localX, long size) {
-    return unproject(x, zoom, boundX, localX, size);
-}
-
-long double Fractal::yToImaginary(long y, long zoom, long localY, long size) {
-    return unproject(y, zoom, boundY, localY, size);
 }
 
