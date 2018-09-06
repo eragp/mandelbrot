07040a36cecd7b561795e18ca2d3fe881e76db3b
Max Fruehauf <max.fruehauf@tum.de>
2018-09-06 18:08:22 +0200

    Revert "refactor NodeProgress to typeScript"
    This reverts commit df665dba532163677e056da4fe7460e346f2897b.

--- frontend/src/visualization/NodeProgress.tsx
+++ frontend/src/visualization/NodeProgress.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Chart, ChartConfiguration } from "chart.js";
+import { Chart } from "chart.js";
 import WebSocketClient from "../connection/WSClient";
 
 import "./NodeProgress.css";
@@ -9,50 +9,31 @@ export interface NodeProgressProps {
   workerContext: WorkerContext;
   wsClient: WebSocketClient;
 }
-
-interface ChartState {
-  workers: Worker[];
-}
-interface Worker {
-  rank: number;
-  name: string;
-  animating: boolean;
-  computationTime: number;
-}
 /**
  * Shows the computation time of invoked workers
  * Additional documentation on the type of used chart: https://www.chartjs.org/docs/latest/
  */
 export default class NodeProgress extends React.Component<NodeProgressProps, {}> {
   private websocketClient: WebSocketClient;
-  private workerContext: WorkerContext;
-  private chartState: ChartState;
-  private chart: Chart;
-
-  private hoveredItem: any;
-  private redraw: NodeJS.Timer;
+  private chartState: any;
 
   constructor(props: NodeProgressProps) {
     super(props);
     this.websocketClient = props.wsClient;
-    this.workerContext = props.workerContext;
+    console.log(props);
     this.chartState = {
-      workers: [
-        {
-          rank: 0,
-          name: "Worker 0",
-          animating: false,
-          computationTime: 1
-        }
-      ]
+      nodes: [0],
+      active: new Map([[0, false]]),
+      // The computation time in microseconds
+      progress: new Map([[0, 1]])
     };
   }
 
   componentDidMount() {
-    const canvas = document.getElementById("nodeProgress") as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-    const renderLabel = (tooltipItem: any, data: any) => {
+    const ctx = document.getElementById("nodeProgress");
+    const customLabel = (tooltipItem, data) => {
       let label = data.labels[tooltipItem.index];
+
       if (label) {
         label += ": ";
       } else {
@@ -70,7 +51,7 @@ export default class NodeProgress extends React.Component<NodeProgressProps, {}>
         maintainAspectRatio: false,
         tooltips: {
           callbacks: {
-            label: renderLabel
+            label: customLabel
           }
         },
         title: {
@@ -79,21 +60,21 @@ export default class NodeProgress extends React.Component<NodeProgressProps, {}>
           text: ["Total node computation time:", "0 µs"]
         },
         onHover: event => {
-          // change workerContext active worker on hover
+          // change workercontext active worker on hover
           const data = this.chart.getElementsAtEvent(event)[0];
           if (data) {
-            this.workerContext.setActiveWorker(this.chartState.workers[data._index].rank);
-            this.hoveredItem = data;
-          } else if (this.hoveredItem) {
-            this.workerContext.setActiveWorker(undefined);
-            this.hoveredItem = undefined;
+            this.props.workerContext.setActiveWorker(this.chartState.nodes[data._index]);
+            this._hoveredItem = data;
+          } else if (this._hoveredItem) {
+            this.props.workerContext.setActiveWorker(undefined);
+            this._hoveredItem = undefined;
           }
         }
       }
-    } as ChartConfiguration);
-
+    });
     this.updateChart();
-    this.startProgressAnimation();
+
+    this.initNodeProgress();
 
     // register workers at websocket client
     // so that they are set inactive when the first tile/region
@@ -101,58 +82,54 @@ export default class NodeProgress extends React.Component<NodeProgressProps, {}>
     this.websocketClient.registerRegionData(data => {
       // Stop corresponding worker progress bar
       // assume that regionData is passed here
-      const workerID = data.workerInfo.rank as number;
-      var worker = this.chartState.workers.find(w => w.rank === workerID);
-      if (worker === undefined || !worker.animating) {
-        return;
-      }
+      const workerID = data.workerInfo.rank;
       // Pay attention here that ranks begin from 1 as long as the host does not send data on his own
-      worker.animating = false;
+      this.chartState.active.set(workerID, false);
       // insert correct µs time in node value
-      worker.computationTime = data.workerInfo.computationTime;
-      this.updateChart(1000);
-      console.log(`Stopped Worker ${workerID}`);
+      this.chartState.progress.set(workerID, data.workerInfo.computationTime);
+      this.updateChart(0);
     });
 
     this.websocketClient.registerRegion(data => {
       // Stop redrawing
-      this.stopProgressAnimation();
+      this.stopNodeProgress();
       // Reset node progress
-      var workers: Worker[] = [];
+      const nodes = [];
+      const active = new Map();
+      const progress = new Map();
 
-      const animationDuration = 750;
+      let animationDuration = 750;
       for (let region of data.regions) {
-        workers.push({
-          rank: region.nodeID,
-          name: "Worker " + region.nodeID,
-          animating: true,
-          computationTime: animationDuration * 1000
-        });
+        nodes.push(region.nodeID);
+        active.set(region.nodeID, true);
+        progress.set(region.nodeID, animationDuration * 1000);
       }
       this.chartState = {
-        workers: workers
+        nodes: nodes,
+        active: active,
+        progress: progress
       };
       this.updateChart(animationDuration);
       // Start redrawing as soon as animation has finished
       setTimeout(() => {
-        this.startProgressAnimation();
+        this.initNodeProgress();
       }, animationDuration);
     });
 
-    // Highlight segment on active worker change
+    // Highlight segement on active worker change
     // Inspired by https://github.com/chartjs/Chart.js/issues/1768
     this.props.workerContext.subscribe(activeWorker => {
       // Activate new tooltip if necessary
       if (activeWorker !== undefined) {
-        const workerIndex = this.chartState.workers.findIndex(w => w.rank === activeWorker);
+        const workerIndex = this.chartState.nodes.indexOf(activeWorker);
         const activeSegment = this.chart.data.datasets[0]._meta[1].data[workerIndex];
         this.chart.tooltip.initialize();
         this.chart.tooltip._active = [activeSegment];
         this.chart.data.datasets[0]._meta[1].controller.setHoverStyle(activeSegment);
-        this.hoveredItem= activeSegment;
+        this._hoveredSegment = activeSegment;
       } else {
         // Remove tooltip
-        this.chart.data.datasets[0]._meta[1].controller.removeHoverStyle(this.hoveredItem);
+        this.chart.data.datasets[0]._meta[1].controller.removeHoverStyle(this._hoveredSegment);
         this.chart.tooltip._active = [];
       }
       // Update chart
@@ -169,15 +146,15 @@ export default class NodeProgress extends React.Component<NodeProgressProps, {}>
     );
   }
 
-  private updateChart(animationDuration: number = 0) {
-    var labels: string[] = [];
-    var values: number[] = [];
-    var colorSet: string[] = [];
+  updateChart(animationDuration) {
+    const labels = [];
+    const values = [];
+    const colorSet = [];
     // => Label/ value index is the index of the rank in the node array
-    this.chartState.workers.forEach(worker => {
-      labels.push(worker.name);
-      colorSet.push(this.workerContext.getWorkerColor(worker.rank));
-      values.push(worker.computationTime);
+    this.chartState.nodes.forEach(rank => {
+      labels.push("Worker " + rank);
+      colorSet.push(this.props.workerContext.getWorkerColor(rank));
+      values.push(this.chartState.progress.get(rank));
     });
 
     const data = {
@@ -192,29 +169,30 @@ export default class NodeProgress extends React.Component<NodeProgressProps, {}>
     this.chart.data = data;
 
     let computationTime = 0;
-    this.chartState.workers.forEach(worker => {
-      computationTime += worker.computationTime;
+    this.chartState.progress.forEach(value => {
+      computationTime += value;
     });
     this.chart.options.title.text[1] = computationTime + " µs";
+
     this.chart.update(animationDuration);
   }
 
   /**
    * Start redrawing the current node computation time every 50 milliseconds
    */
-  private startProgressAnimation() {
+  initNodeProgress() {
     // Interval in milliseconds
     const intervalRate = 50;
-    this.redraw = setInterval(
+    this.interval = setInterval(
       state => {
-        let updated = false;
-        state.workers.forEach((worker: any) => {
-          if (worker.animating) {
-            worker.computationTime += intervalRate * 1000;
-            updated = true;
+        let update = false;
+        state.progress.forEach((value, rank) => {
+          if (state.active.get(rank)) {
+            state.progress.set(rank, value + intervalRate * 1000);
+            update = true;
           }
         });
-        if (updated) {
+        if (update) {
           // Animation duration of 0 for fluent redrawing
           this.updateChart(0);
         }
@@ -227,7 +205,7 @@ export default class NodeProgress extends React.Component<NodeProgressProps, {}>
   /**
    * Stop redrawing the node progress every 50 milliseconds
    */
-  private stopProgressAnimation() {
-    clearInterval(this.redraw);
+  stopNodeProgress() {
+    clearInterval(this.interval);
   }
 }
