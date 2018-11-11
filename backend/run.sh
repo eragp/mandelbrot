bf6bb610cb053621078d9db70f9296f4d843438d
Max Fruehauf <max.fruehauf@tum.de>
2018-11-11 20:12:04 +0100

    fix docker image versioning

--- backend/run.sh
+++ backend/run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+DOCKER_IMAGE=rasaford/mandelbrot
+IMAGE_VERSION=0.8
+
 echo "Starting the Build Process"
 ## Docker ##
 # check for new base image
-docker pull rasaford/mandelbrot:latest
+docker pull $DOCKER_IMAGE:$IMAGE_VERSION
 # remove old images
 docker rmi $(docker images --filter "dangling=true" -q)
 docker rm $(docker ps -aq -f "status=exited")
-docker build -t mandelbrot:0.1 .
+docker build -t $DOCKER_IMAGE:$IMAGE_VERSION .
 # this needs to be the last command
 # beacause it attaches the current shell to the running container
 docker run --rm -it \
 	--name mandelbrot_build \
 	-p 8080:80 \
 	-p 9002:9002 \
-	mandelbrot:0.1 
+	$DOCKER_IMAGE:$IMAGE_VERSION
