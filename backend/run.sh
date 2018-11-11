4fd75432dee35361a36f5eef6eeea06815e9032e
Max Fruehauf <max.fruehauf@tum.de>
2018-11-11 20:37:16 +0100

    update Docker image version

--- backend/run.sh
+++ backend/run.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-DOCKER_IMAGE=rasaford/mandelbrot
-IMAGE_VERSION=0.8
+DOCKER_REMOTE_IMAGE_NAME="rasaford/mandelbrot"
+IMAGE_VERSION="0.8"
+LOCAL_IMAGE_NAME="mandelbrot_build"
+LOCAL_VERSION="1"
 
 echo "Starting the Build Process"
 ## Docker ##
 # check for new base image
-docker pull $DOCKER_IMAGE:$IMAGE_VERSION
+docker pull $DOCKER_REMOTE_IMAGE_NAME:$IMAGE_VERSION
 # remove old images
 docker rmi $(docker images --filter "dangling=true" -q)
 docker rm $(docker ps -aq -f "status=exited")
-docker build -t $DOCKER_IMAGE:$IMAGE_VERSION .
+docker build -t $LOCAL_IMAGE_NAME:$LOCAL_VERSION .
 # this needs to be the last command
 # beacause it attaches the current shell to the running container
 docker run --rm -it \
 	--name mandelbrot_build \
 	-p 8080:80 \
 	-p 9002:9002 \
-	$DOCKER_IMAGE:$IMAGE_VERSION
+	$LOCAL_IMAGE_NAME:$LOCAL_VERSION
