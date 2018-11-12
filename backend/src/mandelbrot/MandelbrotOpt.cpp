125ca4ef7f0e58a713166b1ddad66714a16b6d54
Niels Mündler <n.muendler@tum.de>
2018-11-12 10:41:08 +0100

    Attempt at an optimizable version of mandelbrot compuatation
    Test with compiler and optimization flags

---
+++ backend/src/mandelbrot/MandelbrotOpt.cpp
@@ -0,0 +1,55 @@
+#include "Mandelbrot.h"
+#include <algorithm>
+
+/**
+ * Return whether at least one point still has an abs value below 2 => continue computation
+ * @param dest Memory where it is stored whether this point is still computed (0 for no, 1 for yes)
+ */
+bool continueComp(long double* zReal, long double* zImaginary, int vectorLength, int* dest){
+    bool lessThanTwo = false;
+    for(int k = 0; k < vectorLength; k++){
+        dest[k] = zReal[k] * zReal[k] + zImaginary[k] * zImaginary[k] < 4.0 ? 1 : 0;
+        // or => if any value is true, true is returned
+        lessThanTwo = lessThanTwo || (dest[k] > 0);
+    }
+    return lessThanTwo;
+}
+
+// Probably more open to compiler optimization
+void Mandelbrot::calculateFractal(long double* cReal, long double* cImaginary, int maxIteration, int vectorLength, int* dest) {
+    std::fill(dest, dest+vectorLength, 0);
+    int i = 0;
+    long double zReal[vectorLength] = {0.0};
+    long double zImaginary[vectorLength] = {0.0};
+    long double nextZReal[vectorLength];
+    long double nextZImaginary[vectorLength];
+    // Factor that is multiplied on iteration count and computation 
+    // is 0 or 1 and determines whether that point is still being computed
+    int factor[vectorLength] = {1};
+    // Bool storing information about whether any abs value that is being computed
+    // is still below two => continue computation
+    bool lessThanTwo = continueComp(zReal, zImaginary, vectorLength, factor);
+    while (i < maxIteration && lessThanTwo){
+        for(int k = 0; k < vectorLength; k++){
+            nextZReal[k] = (zReal[k] * zReal[k] - zImaginary[k] * zImaginary[k]) + cReal[k];
+            nextZImaginary[k] = 2 * (zReal[k] * zImaginary[k]) + cImaginary[k];
+            zReal[k] = factor[k]*nextZReal[k];
+            zImaginary[k] = factor[k]*nextZImaginary[k];
+            dest[k] += factor[k];
+        }
+    }
+}
+
+int Mandelbrot::calculateFractalNonParallel(long double cReal, long double cImaginary, int maxIteration){
+    int i = 0;
+    long double zReal = 0.0;
+    long double zImaginary = 0.0;
+    while (i < maxIteration && zReal * zReal + zImaginary * zImaginary < 4.0) {
+        long double nextZReal = (zReal * zReal - zImaginary * zImaginary) + cReal;
+        long double nextZImaginary = 2 * (zReal * zImaginary) + cImaginary;
+        zReal = nextZReal;
+        zImaginary = nextZImaginary;
+        i++;
+    }
+    return i;
+}
