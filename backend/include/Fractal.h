839b40b0c3e2e5e4d775f8a229625e5cae83f445
Niels Mündler <n.muendler@tum.de>
2018-12-15 16:43:21 +0100

    Make precision variable and global and implement SIMDable version of mandelbrot

--- backend/include/Fractal.h
+++ backend/include/Fractal.h
@@ -1,4 +1,8 @@
 #pragma once
+
+// Define the precision for computations globally
+typedef long double precision_t;
+
 class Fractal {
    public:
     /**
@@ -9,7 +13,7 @@ class Fractal {
      * @param vectorLength number of coordinates to be calculated at once
      * @param dest memory position where result(number of iterations for coordinates) should be stored
      */
-    virtual void calculateFractal(long double* cReal, long double* cImaginary, int maxIteration, int vectorLength, int* dest) = 0;
+    virtual void calculateFractal(precision_t* cReal, precision_t* cImaginary, int maxIteration, int vectorLength, int* dest) = 0;
     virtual ~Fractal();
 
     /**
@@ -19,7 +23,7 @@ class Fractal {
      * @param xRes pixel resolution from minReal -> maxReal
      * @return step size on real axis
      */
-    static double deltaReal(double maxReal, double minReal, int xRes);
+    static precision_t deltaReal(precision_t maxReal, precision_t minReal, int xRes);
 
     /**
      * Calculates the step size of a single pixel in the y direction (imaginary) on the complex plane.
@@ -28,5 +32,5 @@ class Fractal {
      * @param yRes pixel resolution from minImaginary -> maxImaginary
      * @return step size on imaginary axis
      */
-    static double deltaImaginary(double maxImaginary, double minImaginary, int yRes);
+    static precision_t deltaImaginary(precision_t maxImaginary, precision_t minImaginary, int yRes);
 };
