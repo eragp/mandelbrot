8bef8d57074650d61b14620447eeb0dd0f7f71ae
Niels Mündler <n.muendler@tum.de>
2018-12-23 20:46:55 +0100

    Non functional recursive naive balancer

--- backend/src/balancer/NaiveBalancer.cpp
+++ backend/src/balancer/NaiveBalancer.cpp
@@ -6,101 +6,64 @@
 #include <iostream>
 
 const std::string NaiveBalancer::NAME = "naive";
+const Region emptyRegion = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
+void balanceLoadRecursively(Region region, int nodeCount, Region* dest){
+    std::cout << "Slicing region "
+              << " TopLeft: (" << region.minReal << ", " << region.maxImaginary << ") -> BottomRight: ("
+              << region.maxReal << ", " << region.minImaginary << ") Resolution: ("
+              << region.width << ", " << region.height << ") Position: (" <<region.hOffset << ", " << region.vOffset << ")"<< std::endl;
+
+    unsigned int gD = region.guaranteedDivisor;
+    unsigned int halvWidth = region.width/2;
+    unsigned int halvHeight = region.height/2;
+    // Note that guaranteed divisor should divide the height and width from the beginning on
+    if(nodeCount == 1 || (halvWidth < gD && halvHeight < gD)){
+        // We cannot divide this region any further
+        // so we write the current region and empty regions for the rest
+        dest[0] = region;
+        for(int i = 1; i < nodeCount; i++){
+            dest[i] = emptyRegion;
+        }
+        return;
+    }
+    Region firstRegion = region;
+    Region secondRegion = region;
+    // Determine by width/height how to divide 
+    // A division is only appropiate if the resulting halve is still greater than gD
+    if(halvWidth >= gD && region.width >= region.height){
+        firstRegion.width = halvWidth;
+        secondRegion.width = halvWidth;
+        long double midReal = (region.minReal + region.maxReal) / 2;
+        firstRegion.maxReal = midReal;
+        secondRegion.minReal = midReal;
+        secondRegion.hOffset = region.hOffset + halvWidth;
+    }
+    else if(halvHeight >= gD){
+        firstRegion.height = halvHeight;
+        secondRegion.height = halvHeight;
+        long double midImag = (region.minImaginary + region.maxImaginary) / 2;
+        firstRegion.minImaginary = midImag;
+        secondRegion.maxImaginary = midImag;
+        secondRegion.vOffset = region.vOffset + halvHeight;
+    }
+    else{
+        std::cout << "Height of " << halvHeight << ", still slicing? gd: " << region.guaranteedDivisor << std::endl;
+    }
+    int halvNodes = nodeCount/2;
+    balanceLoadRecursively(firstRegion, halvNodes, dest);
+    balanceLoadRecursively(secondRegion, nodeCount-halvNodes, dest+halvNodes);
+}
+
+// Recursively divides region in half and balances load onto them
 Region* NaiveBalancer::balanceLoad(Region region, int nodeCount)
 {
-
-	Region* ret = new Region[nodeCount];
-
-	int xQuantity = (int)floor(sqrt(nodeCount));		// xQuantity = Quantity of separate parts in x direction --> Number of Columns 
-	while (nodeCount % xQuantity != 0) {				// Find the next integral divisor of nodeCount smaller than its squareroot
-		xQuantity--;
-	}
-
-	int yQuantity = nodeCount / xQuantity;	// nodeCount = xQuantity * yQuantity; --> Number of Rows
-
-	// Define an empty region
-	Region empty;
-	empty.minImaginary = 0.0;
-	empty.maxImaginary = 0.0;
-
-	empty.minReal = 0.0;
-	empty.maxReal = 0.0;
-
-	empty.height = 0;
-	empty.width = 0;
-
-	empty.vOffset = 0;
-	empty.hOffset = 0;
-
-	empty.maxIteration = region.maxIteration;
-	empty.validation = region.validation;
-	empty.guaranteedDivisor = region.guaranteedDivisor;
-
-	int partWidth = (region.width / (region.guaranteedDivisor * xQuantity)) * region.guaranteedDivisor;	// Split Resolution for x, multiple of guaranteedDivisor
-	
-	if (partWidth <= 0) {
-		partWidth = region.guaranteedDivisor;
-		int spareCols = xQuantity - (region.width / region.guaranteedDivisor);
-		// Set affected regions to empty
-		for (int i = 0; i < spareCols; i++) {
-			for (int j = 0; j < yQuantity; j++) {
-				ret[(xQuantity - 1 - i) * yQuantity + j] = empty;
-			}
-		}
-		xQuantity -= spareCols;
-	}
-
-	int partHeight = (region.height / (region.guaranteedDivisor * yQuantity)) * region.guaranteedDivisor;	// Split Resolution for y, multiple of guaranteedDivisor
-
-	if (partHeight <= 0) {
-		partHeight = region.guaranteedDivisor;
-		int spareRows = yQuantity - (region.height / region.guaranteedDivisor);
-		// Set affected regions to empty
-		for (int i = 0; i < spareRows; i++) {
-			for (int j = 0; j < xQuantity; j++) {
-				ret[j * yQuantity + (yQuantity - 1 - i)] = empty;
-			}
-		}
-		yQuantity -= spareRows;
-	}
-
-	double xDelta = ((region.maxReal - region.minReal) / region.width) * partWidth;		// xDelta = x dimension of every region (except maybe the last) this method returns
-
-	double yDelta = ((region.maxImaginary - region.minImaginary) / region.height) * partHeight;	//  yDelta = y dimension of every region (except maybe the last) this method returns
-
-	int retPos = 0;	// Current position in the Array ret
-	for (int x = 0; x < xQuantity; x++) {
-		for (int y = 0; y < yQuantity; y++) {
-			ret[retPos].maxIteration = region.maxIteration;
-			ret[retPos].validation = region.validation;
-			ret[retPos].guaranteedDivisor = region.guaranteedDivisor;
-
-			ret[retPos].minReal = region.minReal + x * xDelta;
-			ret[retPos].maxImaginary = region.maxImaginary - y * yDelta;
-
-			ret[retPos].maxReal = region.minReal + (x + 1) * xDelta;
-			ret[retPos].minImaginary = region.maxImaginary - (y + 1) * yDelta;
-
-			ret[retPos].width = partWidth;
-			ret[retPos].height = partHeight;
-
-			ret[retPos].hOffset = x * partWidth;
-			ret[retPos].vOffset = y * partHeight;
-
-			// We want the same resolution as before, so the last part could be a bit larger
-			if (x + 1 == xQuantity) {
-				ret[retPos].width = region.width - ((xQuantity - 1) * partWidth);
-				ret[retPos].maxReal = region.maxReal;
-			}
-
-			if (y + 1 == yQuantity) {
-				ret[retPos].height = region.height - ((yQuantity - 1) * partHeight);
-				ret[retPos].minImaginary = region.minImaginary;
-			}
-			retPos++;
-		}
-	}
-
-	return ret;
+    Region* result = new Region[nodeCount];
+    if(region.guaranteedDivisor == 0){
+        region.guaranteedDivisor = 1;
+    }
+    if(nodeCount > 0){
+        balanceLoadRecursively(region, nodeCount, result);
+    }
+    return result;
 }
