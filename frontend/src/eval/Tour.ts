f5b2652fc57cd01615e46e57559c6ab9c5f586ee
Max Fruehauf <max.fruehauf@tum.de>
2018-12-29 15:04:12 +0100

    Asynchronous Tour working

--- frontend/src/eval/Tour.ts
+++ frontend/src/eval/Tour.ts
@@ -1,15 +1,9 @@
-import WebSocketClient, { WS } from "../connection/WSClient";
-import { RegionData } from "../connection/ExchangeTypes";
-import { RegionGroup } from "../misc/RegionGroup";
 import {
   BalancerObservable,
-  GroupObservable,
   ImplementationObservable,
   ViewCenterObservable
 } from "../misc/Observable";
-import { setURLParams } from "../misc/URLParams";
 import { Point3D } from "../misc/Point";
-import { registerCallback } from "../misc/registerCallback";
 import { StatsCollector } from "./StatsCollector";
 
 interface Tour {
@@ -37,7 +31,7 @@ interface Output {
 interface Run {
   balancer: string;
   implementation: string;
-  data: Datapoint[];
+  data: Datapoint;
 }
 interface Datapoint {
   poi: PoI;
@@ -49,36 +43,19 @@ interface BalancerTime {
   emptyRegions: number;
 }
 interface WorkerTime {
-  id: number;
+  rank: number;
   computationTime: number;
   mpiTime: number;
   networkTime: number;
   drawTime: number;
 }
 
-const run: Run = {
-  balancer: "naive",
-  implementation: "default",
-  data: [
-    {
-      poi: { real: 0, imag: 0, zoom: 0 },
-      balancer: {
-        time: 0,
-        emptyRegions: 0
-      },
-      workers: [
-        {
-          id: 0,
-          computationTime: 1,
-          mpiTime: 1,
-          networkTime: 1,
-          drawTime: 1
-        }
-      ]
-    }
-  ]
-};
-
+interface Config {
+  balancer: string;
+  impl: string;
+  poi: PoI;
+}
+let configs: Config[] = [];
 export const startTour = (
   stats: StatsCollector,
   viewCenter: ViewCenterObservable,
@@ -88,17 +65,45 @@ export const startTour = (
   const tour = require("./config.json") as Tour;
 
   const out: Output = { config: tour, datapoints: [] };
-  for (const balancerType of tour.balancers) {
-    balancer.set(balancerType);
-    for (const implType of tour.implementations) {
-      impl.set(implType);
+  for (const balancer of tour.balancers) {
+    for (const impl of tour.implementations) {
       for (const poi of tour.pois) {
-        console.log("set center ", poi);
-        viewCenter.set(new Point3D(poi.real, poi.imag, poi.zoom));
+        configs.push({ balancer, impl, poi });
       }
     }
   }
+  // console.log("set center ", poi);
+  // viewCenter.set(new Point3D(poi.real, poi.imag, poi.zoom));
 
-  console.log(out);
+  const cont = (output: Output) => {
+    if (configs.length === 0) {
+      console.log(output);
+      return;
+    }
+    const c = configs.pop() as Config;
+    configs = configs.slice(0, configs.length - 1);
+
+    balancer.set(c.balancer);
+    impl.set(c.impl);
+    let p = new Point3D(c.poi.real, c.poi.imag, c.poi.zoom);
+    viewCenter.set(p);
+
+    stats.registerDone(stats => {
+      out.datapoints.push({
+        balancer: c.balancer,
+        implementation: c.impl,
+        data: {
+          poi: c.poi,
+          balancer: {
+            time: 0,
+            emptyRegions: 0
+          },
+          workers: Array.from(stats.worker.values())
+        }
+      });
+      cont(out);
+    });
+  };
+  cont(out);
   // const str = JSON.stringify(out);
 };
