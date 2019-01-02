61748b8f163370cf463c3ea26648544fc6f8dd8d
Max Fruehauf <max.fruehauf@tum.de>
2019-01-02 16:46:02 +0100

    Create README.md and install script for venv

--- eval/create_env.sh
+++ eval/create_env.sh
@@ -1,6 +1,1 @@
-
 conda create -n mandelbrot_eval python=3.7.1 ipykernel pip
-pip install -r "requirements.txt"
-# install ipython kernels for jupyter notebook
-ipython kernel install
-ipython3 kernel install
