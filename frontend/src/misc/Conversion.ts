c942c2217cf9803f74c463672d61752e22a2437a
Max Fruehauf <max.fruehauf@tum.de>
2019-01-17 22:27:16 +0100

    Fix time display

--- frontend/src/misc/Conversion.ts
+++ frontend/src/misc/Conversion.ts
@@ -1,10 +1,10 @@
-export const usToString = (timeus: number) => {
+export const usToString = (t: number) => {
   const units = ["μs", "ms", "s", "min", "hr", "day", "week"];
-  const divisors = [1000, 1000, 1000, 60, 60, 24, 7];
+  const divisors = [1000, 1000, 60, 60, 24, 7];
   let i = 0;
-  while (i !== units.length && timeus > 1000) {
-    timeus = timeus / divisors[i];
+  while (i !== units.length && t > divisors[i]) {
+    t = t / divisors[i];
     i++;
   }
-  return `${(Math.round(timeus * 100) / 100).toFixed(2)} ${units[i]}`;
+  return `${(Math.round(t * 100) / 100).toFixed(2)} ${units[i]}`;
 };
